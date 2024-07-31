4ff6ed59d72b7947b8f94cc49f63b199f25e3f3a
Ali Almasi <a710almasi@gmail.com>
2024-07-31 22:13:33 +0330

    sql update

--- iranianshop.sql
+++ iranianshop.sql
@@ -1,99 +1,59 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Jul 31, 2024 at 04:33 PM
--- Server version: 5.7.36
--- PHP Version: 7.4.26
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+03:30";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `iranianshop`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `messages`
---
+CREATE DATABASE IF NOT EXISTS `iranianshop` DEFAULT CHARACTER SET utf8 COLLATE utf8_persian_ci;
+USE `iranianshop`;
 
 DROP TABLE IF EXISTS `messages`;
 CREATE TABLE IF NOT EXISTS `messages` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'کد پیام',
-  `realname` varchar(70) COLLATE utf8_persian_ci NOT NULL COMMENT 'نام کامل',
-  `email` varchar(50) COLLATE utf8_persian_ci NOT NULL COMMENT 'پست الکترونیک',
-  `content` text COLLATE utf8_persian_ci NOT NULL COMMENT 'متن پیام',
+  `realname` varchar(70) NOT NULL COMMENT 'نام کامل',
+  `email` varchar(50) NOT NULL COMMENT 'پست الکترونیک',
+  `content` text NOT NULL COMMENT 'متن پیام',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci COMMENT='جدول پیامها';
 
---
--- Dumping data for table `messages`
---
-
+TRUNCATE TABLE `messages`;
 INSERT INTO `messages` (`id`, `realname`, `email`, `content`) VALUES
 (1, 'Ø¹Ù„ÛŒ Ø§Ù„Ù…Ø§Ø³ÛŒ', 'a710almasi@gmail.com', 'Ø³Ù„Ø§Ù… Ùˆ Ø®Ø³ØªÙ‡ Ù†Ø¨Ø§Ø´ÛŒØ¯. Ù…Ù…Ù†ÙˆÙ† Ø§Ø² Ø³Ø§ÛŒØª Ø®ÙˆØ¨ØªÙˆÙ†.'),
 (2, 'Ø§Ø±Ø³Ù„Ø§Ù† Ø§Ù„Ù…Ø§Ø³ÛŒ', 'arsalan@almasi.ir', 'Ø³Ù„Ø§Ù…. Ù…ÛŒØ®ÙˆØ§Ø³ØªÙ… Ø¯Ø±Ù…ÙˆØ±Ø¯ Ø³ÙØ§Ø±Ø´Ù… Ù¾ÛŒÚ¯ÛŒØ±ÛŒ Ú©Ù†Ù…. Ú©Ø¯ Ø³ÙØ§Ø±Ø´ Ø¨Ù†Ø¯Ù‡ 1478536291 Ù‡Ø³ØªØ´. Ù…Ù…Ù†ÙˆÙ†Ù….');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE IF NOT EXISTS `orders` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'شماره سفارش',
-  `username` varchar(30) COLLATE utf8_persian_ci NOT NULL COMMENT 'نام کاربری سفارش دهنده',
-  `orderdate` varchar(20) COLLATE utf8_persian_ci NOT NULL COMMENT 'تاریخ سفارش',
+  `username` varchar(30) NOT NULL COMMENT 'نام کاربری سفارش دهنده',
+  `orderdate` varchar(20) NOT NULL COMMENT 'تاریخ سفارش',
   `pro_id` int(10) NOT NULL COMMENT 'کد کالا',
   `pro_qty` int(10) NOT NULL COMMENT 'تعداد سفارش',
   `pro_price` float NOT NULL COMMENT 'قیمت پایه کالا',
-  `number` varchar(11) COLLATE utf8_persian_ci NOT NULL COMMENT 'شماره تماس',
-  `address` varchar(400) COLLATE utf8_persian_ci NOT NULL COMMENT 'آدرس',
-  `trackcode` varchar(24) COLLATE utf8_persian_ci NOT NULL COMMENT 'کد مرسوله پستی',
+  `number` varchar(11) NOT NULL COMMENT 'شماره تماس',
+  `address` varchar(400) NOT NULL COMMENT 'آدرس',
+  `trackcode` varchar(24) NOT NULL COMMENT 'کد مرسوله پستی',
   `state` int(1) NOT NULL COMMENT 'وضعیت سفارش',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci COMMENT='جدول سفارشات';
 
---
--- Dumping data for table `orders`
---
-
+TRUNCATE TABLE `orders`;
 INSERT INTO `orders` (`id`, `username`, `orderdate`, `pro_id`, `pro_qty`, `pro_price`, `number`, `address`, `trackcode`, `state`) VALUES
 (1, 'almasi', '1403/05/10', 9, 7, 120000, '09306178128', 'ØªÙ‡Ø±Ø§Ù†ØŒ Ø´Ù‡Ø±Ø§Ù†ØŒ Ø®ÛŒØ§Ø¨Ø§Ù† Ø¬Ù‡Ø§Ø¯ØŒ Ù¾Ù„Ø§Ú© 300 ÙˆØ§Ø­Ø¯ 8', '145543342956394292759822', 1),
 (2, 'arsalan', '1403/05/11', 6, 2, 140000, '09194809301', 'ØªÙ‡Ø±Ø§Ù†ØŒ Ø¨Ù„ÙˆØ§Ø± Ø´Ù‡Ø¯Ø§ØŒ Ø®ÛŒØ§Ø¨Ø§Ù† Ú©Ø§Ø¬ØŒ Ø±Ùˆ Ø¨Ù‡ Ø±ÙˆÛŒ Ø¯Ø¨ÛŒØ±Ø³ØªØ§Ù† Ø§Ù„ÙˆÙ†Ø¯ Ù¾Ù„Ø§Ú© 18 ÙˆØ§Ø­Ø¯ 20', '727993337375246779695843', 0);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
-
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'کد کالا',
-  `name` varchar(200) COLLATE utf8_persian_ci NOT NULL COMMENT 'نام کالا',
+  `name` varchar(200) NOT NULL COMMENT 'نام کالا',
   `qty` int(10) NOT NULL COMMENT 'موجودی کالا',
   `price` float NOT NULL COMMENT 'قیمت کالا',
-  `image` varchar(80) COLLATE utf8_persian_ci NOT NULL COMMENT 'عکس کالا',
-  `details` text COLLATE utf8_persian_ci NOT NULL COMMENT 'توضیحات کالا',
+  `image` varchar(80) NOT NULL COMMENT 'عکس کالا',
+  `details` text NOT NULL COMMENT 'توضیحات کالا',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1118 DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci COMMENT='جدول کالاها';
 
---
--- Dumping data for table `products`
---
-
+TRUNCATE TABLE `products`;
 INSERT INTO `products` (`id`, `name`, `qty`, `price`, `image`, `details`) VALUES
 (1, 'Ø­Ù‚Ù‡ Ú†ÙˆØ¨ÛŒ', 1, 250000, 'hoghe-choobi.jpg', 'Ø§ÛŒÙ† Ø­Ù‚Ù‡ Ú†ÙˆØ¨ÛŒ Ø¨Ø§Ø± Ø¹Ø¸ÛŒÙ…ÛŒ Ø¨Ù‡ Ø¯ÙˆØ´ Ù…ÛŒÚ©Ø´Ø¯ØŒ Ù‡Ù†Ø± Ø§ØµÛŒÙ„ Ùˆ Ø¨Ø§Ø³ØªØ§Ù†ÛŒ Ø§ÛŒØ±Ø§Ù†ÛŒ.  Ù…Ø¯Ø§Ø¯ Ø±Ù†Ú¯ÛŒ Ù‡Ø§ Ù…Ø´ØºÙˆÙ„ Ø¨ÙˆØ¯Ù†Ø¯ Ø¨Ù‡ Ø¬Ø² Ù…Ø¯Ø§Ø¯ Ø³ÙÛŒØ¯ØŒ Ù‡ÛŒÚ†Ú©Ø³ Ø¨Ù‡ Ø§Ùˆ Ú©Ø§Ø± Ù†Ù…ÛŒØ¯Ø§Ø¯ØŒ Ù‡Ù…Ù‡ Ù…ÛŒÚ¯ÙØªÙ†Ø¯ : ØªÙˆ Ø¨Ù‡ Ù‡ÛŒÚ† Ø¯Ø±Ø¯ÛŒ Ù†Ù…ÛŒØ®ÙˆØ±ÛŒØŒ ÛŒÚ© Ø´Ø¨ Ú©Ù‡ Ù…Ø¯Ø§Ø¯ Ø±Ù†Ú¯ÛŒ Ù‡Ø§ ØªÙˆ Ø³ÛŒØ§Ù‡ÛŒ Ø´Ø¨ Ú¯Ù… Ø´Ø¯Ù‡ Ø¨ÙˆØ¯Ù†Ø¯ØŒ Ù…Ø¯Ø§Ø¯ Ø³ÙÛŒØ¯ ØªØ§ ØµØ¨Ø­ Ù…Ø§Ù‡ Ú©Ø´ÛŒØ¯ Ù…Ù‡ØªØ§Ø¨ Ú©Ø´ÛŒØ¯ Ùˆ Ø§Ù†Ù‚Ø¯Ø± Ø³ØªØ§Ø±Ù‡ Ú©Ø´ÛŒØ¯ Ú©Ù‡ Ú©ÙˆÚ†Ú© Ùˆ Ú©ÙˆÚ†Ú©ØªØ± Ø´Ø¯ ØµØ¨Ø­ ØªÙˆÛŒ Ø¬Ø¹Ø¨Ù‡ Ù…Ø¯Ø§Ø¯ Ø±Ù†Ú¯ÛŒ Ø¬Ø§ÛŒ Ø®Ø§Ù„ÛŒ Ø§Ùˆ Ø¨Ø§ Ù‡ÛŒÚ† Ø±Ù†Ú¯ÛŒ  Ù¾Ø± Ù†Ø´Ø¯ØŒ Ø¨Ù‡ ÛŒØ§Ø¯ Ù‡Ù… Ø¨Ø§Ø´ÛŒÙ… Ø´Ø§ÛŒØ¯ ÙØ±Ø¯Ø§ Ù…Ø§ Ù‡Ù… Ø¯Ø± Ú©Ù†Ø§Ø± Ù‡Ù… Ù†Ø¨Ø§Ø´ÛŒÙ…â€¦'),
 (2, 'Ø­Ù‚Ù‡ Ø¬ÙˆÚ© Ú©Ø§Ø±ÛŒ', 1, 1650000, 'hoghe-jukkari.jpg', 'Ø§ÛŒÙ† Ø­Ù‚Ù‡ Ø­Ø§ÙˆÛŒ Ù‡Ù†Ø± Ø§ØµÛŒÙ„ Ø§ÛŒØ±Ø§Ù†ÛŒ Ø¨Ù‡ Ù†Ø§Ù… Ø¬ÙˆÚ© Ú©Ø§Ø±ÛŒ Ø§Ø³Øª. Ø§ÛŒÙ† Ù‡Ù†Ø± Ø§Ø² Ø³Ø®Øª ØªØ±ÛŒÙ† Ù‡Ù†Ø± Ù‡Ø§ÛŒ Ø§ÛŒØ±Ø§Ù†ÛŒ Ø§Ø³Øª.  Ù…Ø¯Ø§Ø¯ Ø±Ù†Ú¯ÛŒ Ù‡Ø§ Ù…Ø´ØºÙˆÙ„ Ø¨ÙˆØ¯Ù†Ø¯ Ø¨Ù‡ Ø¬Ø² Ù…Ø¯Ø§Ø¯ Ø³ÙÛŒØ¯ØŒ Ù‡ÛŒÚ†Ú©Ø³ Ø¨Ù‡ Ø§Ùˆ Ú©Ø§Ø± Ù†Ù…ÛŒØ¯Ø§Ø¯ØŒ Ù‡Ù…Ù‡ Ù…ÛŒÚ¯ÙØªÙ†Ø¯ : ØªÙˆ Ø¨Ù‡ Ù‡ÛŒÚ† Ø¯Ø±Ø¯ÛŒ Ù†Ù…ÛŒØ®ÙˆØ±ÛŒØŒ ÛŒÚ© Ø´Ø¨ Ú©Ù‡ Ù…Ø¯Ø§Ø¯ Ø±Ù†Ú¯ÛŒ Ù‡Ø§ ØªÙˆ Ø³ÛŒØ§Ù‡ÛŒ Ø´Ø¨ Ú¯Ù… Ø´Ø¯Ù‡ Ø¨ÙˆØ¯Ù†Ø¯ØŒ Ù…Ø¯Ø§Ø¯ Ø³ÙÛŒØ¯ ØªØ§ ØµØ¨Ø­ Ù…Ø§Ù‡ Ú©Ø´ÛŒØ¯ Ù…Ù‡ØªØ§Ø¨ Ú©Ø´ÛŒØ¯ Ùˆ Ø§Ù†Ù‚Ø¯Ø± Ø³ØªØ§Ø±Ù‡ Ú©Ø´ÛŒØ¯ Ú©Ù‡ Ú©ÙˆÚ†Ú© Ùˆ Ú©ÙˆÚ†Ú©ØªØ± Ø´Ø¯ ØµØ¨Ø­ ØªÙˆÛŒ Ø¬Ø¹Ø¨Ù‡ Ù…Ø¯Ø§Ø¯ Ø±Ù†Ú¯ÛŒ Ø¬Ø§ÛŒ Ø®Ø§Ù„ÛŒ Ø§Ùˆ Ø¨Ø§ Ù‡ÛŒÚ† Ø±Ù†Ú¯ÛŒ  Ù¾Ø± Ù†Ø´Ø¯ØŒ Ø¨Ù‡ ÛŒØ§Ø¯ Ù‡Ù… Ø¨Ø§Ø´ÛŒÙ… Ø´Ø§ÛŒØ¯ ÙØ±Ø¯Ø§ Ù…Ø§ Ù‡Ù… Ø¯Ø± Ú©Ù†Ø§Ø± Ù‡Ù… Ù†Ø¨Ø§Ø´ÛŒÙ…â€¦'),
@@ -114,32 +74,22 @@ INSERT INTO `products` (`id`, `name`, `qty`, `price`, `image`, `details`) VALUES
 (17, 'Ø¸Ø±Ù ÙÛŒØ±ÙˆØ²Ù‡ Ú©ÙˆØ¨ÛŒ Ø´Ø¯Ù‡', 2, 1510000, 'zarf-firozekoobi.jpg', 'Ø§Ú¯Ø± Ø¯Ù†Ø¨Ø§Ù„ Ø§Ù„Ù…Ø§Ù† Ø®Ø§ØµÛŒ Ø¨Ø±Ø§ÛŒ Ø¯Ú©ÙˆØ± ÛŒØ§ Ø·Ø±Ø§Ø­ÛŒ Ø¯Ø§Ø®Ù„ÛŒ Ø®Ø§Ù†Ù‡â€ŒØªØ§Ù† Ù‡Ø³ØªÛŒØ¯ØŒ Ø§ÛŒÙ† Ù…Ø­ØµÙˆÙ„ Ù…ÛŒØªÙˆØ§Ù†Ø¯ Ø¨Ø®ÙˆØ¨ÛŒ Ø¨Ø±Ø§ÛŒ Ø´Ù…Ø§ Ø§ÛŒÙ† Ú©Ø§Ø± Ø±Ø§ Ø§Ù†Ø¬Ø§Ù… Ø¯Ù‡Ø¯. Ø®Ø§Ù„Ø¯ Ø­Ø³ÛŒÙ†ÛŒ ØªÙˆ Ø±Ù…Ø§Ù† Ø¨Ø§Ø¯ Ø¨Ø§Ø¯Ú© Ø¨Ø§Ø² Ù…ÛŒÙ†ÙˆÛŒØ³Ù‡ : ï»£ïº®ïº© ïºï»«ïº´ïº˜ï»ª ïº©ïº­ ï®”ï»®ïºµ ï»“ïº®ïº¯ï»§ïºª ïº—ïºŽïº¯ï»© ïº‘ï»ª ïº‘ï» ï»®ï» ïº­ïº³ï¯¿ïºªï»© ïºïºµ ïº‘ïº®ïºï¯¼ ï­˜ï»¨ïºª ï­¼ï»¨ï¯¿ï»¦ ï»§ïº ï»®ïº ï®ïº®ïº© : â€ ï­˜ïº´ïº®ï»¡ ïº©ïº­ ïº¯ï»§ïºªï®”ï¯½ ï»«ïº®ï®”ïº° ïº©ïº¯ïº©ï¯¼ ï»§ï®‘ï»¦ â€ ï­˜ïº´ïº® ï»£ïº˜ï»Œïº ïº ï»­ ï»£ïº’ï»¬ï»®ïº• ïº‘ï»ª ï­˜ïºªïº­ ï»§ï®•ïºŽï»© ï®ïº®ïº© ïº‘ïºªï¯¾ï»¦ ï»£ï»Œï»¨ïºŽ ï®ï»ª ïºï»­ ï»«ïº®ï®”ïº° ïº©ïº³ïº– ï®ïºž ï»§ïºªïºïº·ïº˜ï»ª ï­˜ïºªïº­ ïº‘ï»ª ï»§ï®•ïºŽï»© ï»£ïº˜ï»Œïº ïº ï»“ïº®ïº¯ï»§ïºª ï»Ÿïº’ïº¨ï»¨ïºªï¯¼ ïº¯ïº© ï»­ ïºïº©ïºï»£ï»ª ïº©ïºïº© : ïº©ïº­ ïº¯ï»§ïºªï®”ï¯½ ïº©ïº­ï»­ï» ï»§ï®•ï»® ï­¼ïº®ïº ï®ï»ª ïºï®”ïº® ï®”ï»”ïº˜ï¯½ ïº»ïºªïºï»—ïº– ïº­ïº ïº©ïº¯ïº©ï¯¾ïºªï»© ïºï¯¼ØŒ ïº§ï¯¿ïºŽï»§ïº– ï»§ï®‘ï»¦ ï®ï»ª ïºï®”ïº® ï®ïº®ïº©ï¯¼ ï»‹ïº¸ï»– ïº­ïº ïº©ïº¯ïº©ï¯¾ïºªï»© ïºï¯¼ØŒ ïº§ïº¸ï»®ï»§ïº– ï»§ï®‘ï»¦ ïºï®”ïº® ï®ïº®ïº©ï¯¼ ï»£ïº¤ïº’ïº– ïº­ïº ïº©ïº¯ïº©ï¯¾ïºªï»© ïºï¯¼ØŒ ï»§ïºŽ ïº£ï»– ï»§ï®•ï»® ïºï®”ïº® ï®”ï»”ïº˜ï¯½ ïº£ï»– ïº­ïº ïº©ïº¯ïº©ï¯¾ïºªï»© ïºï¯¼ØŒ ïº‘ï¯½ ïº£ï¯¿ïºŽï¯¾ï¯½ ï»§ï®‘ï»¦ ïºï®”ïº® ï®ïº®ïº©ï¯¼ ïº·ïº®ïºï»“ïº– ïº­ïº ïº©ïº¯ïº©ï¯¾ïºªï»© ïºÛŒ... ï­˜ïº² ïº©ïº­ ïº¯ï»§ïºªï®”ï¯½ ï»“ï»˜ï»‚ ïº©ïº¯ïº©ï¯¼ Ù†Ú©Ù† !\r\n\r\n'),
 (18, 'ÙØ±Ø´ ØªØ¨Ø±ÛŒØ²ÛŒ', 1, 160000000, 'farsh-tabrizi.jpg', 'ÛŒÚ© ÙØ±Ø´ Ø§ØµÛŒÙ„ Ø§ÛŒØ±Ø§Ù†ÛŒØŒ Ù…Ø­ØµÙˆÙ„ Ø¨Ø§Ù†ÙˆØ§Ù† Ù†ÛŒÚ©ÙˆÛŒ ØªØ¨Ø±ÛŒØ²ÛŒ.  Ù…Ø¯Ø§Ø¯ Ø±Ù†Ú¯ÛŒ Ù‡Ø§ Ù…Ø´ØºÙˆÙ„ Ø¨ÙˆØ¯Ù†Ø¯ Ø¨Ù‡ Ø¬Ø² Ù…Ø¯Ø§Ø¯ Ø³ÙÛŒØ¯ØŒ Ù‡ÛŒÚ†Ú©Ø³ Ø¨Ù‡ Ø§Ùˆ Ú©Ø§Ø± Ù†Ù…ÛŒØ¯Ø§Ø¯ØŒ Ù‡Ù…Ù‡ Ù…ÛŒÚ¯ÙØªÙ†Ø¯ : ØªÙˆ Ø¨Ù‡ Ù‡ÛŒÚ† Ø¯Ø±Ø¯ÛŒ Ù†Ù…ÛŒØ®ÙˆØ±ÛŒØŒ ÛŒÚ© Ø´Ø¨ Ú©Ù‡ Ù…Ø¯Ø§Ø¯ Ø±Ù†Ú¯ÛŒ Ù‡Ø§ ØªÙˆ Ø³ÛŒØ§Ù‡ÛŒ Ø´Ø¨ Ú¯Ù… Ø´Ø¯Ù‡ Ø¨ÙˆØ¯Ù†Ø¯ØŒ Ù…Ø¯Ø§Ø¯ Ø³ÙÛŒØ¯ ØªØ§ ØµØ¨Ø­ Ù…Ø§Ù‡ Ú©Ø´ÛŒØ¯ Ù…Ù‡ØªØ§Ø¨ Ú©Ø´ÛŒØ¯ Ùˆ Ø§Ù†Ù‚Ø¯Ø± Ø³ØªØ§Ø±Ù‡ Ú©Ø´ÛŒØ¯ Ú©Ù‡ Ú©ÙˆÚ†Ú© Ùˆ Ú©ÙˆÚ†Ú©ØªØ± Ø´Ø¯ ØµØ¨Ø­ ØªÙˆÛŒ Ø¬Ø¹Ø¨Ù‡ Ù…Ø¯Ø§Ø¯ Ø±Ù†Ú¯ÛŒ Ø¬Ø§ÛŒ Ø®Ø§Ù„ÛŒ Ø§Ùˆ Ø¨Ø§ Ù‡ÛŒÚ† Ø±Ù†Ú¯ÛŒ  Ù¾Ø± Ù†Ø´Ø¯ØŒ Ø¨Ù‡ ÛŒØ§Ø¯ Ù‡Ù… Ø¨Ø§Ø´ÛŒÙ… Ø´Ø§ÛŒØ¯ ÙØ±Ø¯Ø§ Ù…Ø§ Ù‡Ù… Ø¯Ø± Ú©Ù†Ø§Ø± Ù‡Ù… Ù†Ø¨Ø§Ø´ÛŒÙ…â€¦');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `realname` varchar(70) COLLATE utf8_persian_ci NOT NULL COMMENT 'نام واقعی',
-  `username` varchar(30) COLLATE utf8_persian_ci NOT NULL COMMENT 'نام کاربری',
-  `password` varchar(30) COLLATE utf8_persian_ci NOT NULL COMMENT 'رمز عبور',
-  `email` varchar(50) COLLATE utf8_persian_ci NOT NULL COMMENT 'پست الکترونیک',
+  `realname` varchar(70) NOT NULL COMMENT 'نام واقعی',
+  `username` varchar(30) NOT NULL COMMENT 'نام کاربری',
+  `password` varchar(30) NOT NULL COMMENT 'رمز عبور',
+  `email` varchar(50) NOT NULL COMMENT 'پست الکترونیک',
   `u_type` int(1) NOT NULL COMMENT 'نوع کاربر',
   PRIMARY KEY (`username`),
   KEY `username` (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci COMMENT='جدول کاربران';
 
---
--- Dumping data for table `users`
---
-
+TRUNCATE TABLE `users`;
 INSERT INTO `users` (`realname`, `username`, `password`, `email`, `u_type`) VALUES
 ('admin', 'admin', 'admin', 'admin@iranianshop.ir', 1),
 ('Ø¹Ù„ÛŒ Ø§Ù„Ù…Ø§Ø³ÛŒ', 'almasi', 'almasi', 'a710almasi@gmail.com', 0),
 ('Ø§Ø±Ø³Ù„Ø§Ù† Ø§Ù„Ù…Ø§Ø³ÛŒ', 'arsalan', 'almasi', 'arsalan@almasi.ir', 0);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
